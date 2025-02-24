c7f211441c6642ec7a489db7cecd374e04c5f00c
Muhammad Najwan <najwanhassan03@gmail.com>
2025-02-24 13:01:09 +0800

    main.js added

---
+++ main.js
@@ -0,0 +1,4 @@
+function unrepost() {
+    document.getElementById("icon-element-repost").click()
+    document.getElementsByClassName("css-1s9jpf8-ButtonBasicButtonContainer-StyledVideoSwitch e11s2kul11")[0].click();
+}
